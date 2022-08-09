2fe0033e4b9b465ce1bcd9012ea27a3ef68218cb
Rémi Prévost <remi@exomel.com>
2022-08-08 21:06:20 -0400

    Use actual date for disclosure_date

--- lib/elixir_security_advisories/dump.ex
+++ lib/elixir_security_advisories/dump.ex
@@ -1,17 +1,26 @@
 defmodule ElixirSecurityAdvisories.Dump do
   def dump(nodes) do
-    File.rm_rf!("packages")
-
     nodes
     |> Enum.group_by(& &1["advisory"]["ghsaId"])
     |> Enum.map(fn {_, vulnerabilities} ->
       [vulnerability | _] = vulnerabilities
       advisory = vulnerability["advisory"]
 
+      disclosure_date =
+        advisory["publishedAt"]
+        |> case do
+          nil ->
+            ""
+
+          date ->
+            {:ok, disclosure_date, _} = DateTime.from_iso8601(date)
+            to_string(DateTime.to_date(disclosure_date))
+        end
+
       %{
         id: advisory["ghsaId"],
         package: vulnerability["package"]["name"],
-        disclosure_date: advisory["publishedAt"],
+        disclosure_date: disclosure_date,
         cve: advisory["ghsaId"],
         link: advisory["permalink"],
         title: advisory["summary"],
@@ -20,6 +29,7 @@ defmodule ElixirSecurityAdvisories.Dump do
         vulnerable_version_ranges: Enum.map(vulnerabilities, & &1["vulnerableVersionRange"])
       }
     end)
+    |> tap(fn _ -> File.rm_rf!("packages") end)
     |> Enum.map(fn advisory ->
       path = Path.join(["packages", advisory.package, "#{advisory.id}.yml"])
       content = Ymlr.document!(advisory)
