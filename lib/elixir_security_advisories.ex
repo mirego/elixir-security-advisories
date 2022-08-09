51f2de5d9c7d8c53893d055b3d1ffe77b199377b
Rémi Prévost <remi@exomel.com>
2022-08-08 20:50:34 -0400

    Remove main module since it’s not actually used

--- lib/elixir_security_advisories.ex
+++ lib/elixir_security_advisories.ex
@@ -1,18 +1,2 @@
 defmodule ElixirSecurityAdvisories do
-  @moduledoc """
-  Documentation for `ElixirSecurityAdvisories`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirSecurityAdvisories.hello()
-      :world
-
-  """
-  def hello do
-    Application.get_env(:elixir_security_advisories, :github_access_token)
-  end
 end
