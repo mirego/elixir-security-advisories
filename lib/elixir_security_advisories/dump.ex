a54a955c23f774c78ff333d0e89f2a143700f60b
Rémi Prévost <remi@exomel.com>
2022-08-08 21:23:46 -0400

    Improve code

--- lib/elixir_security_advisories/dump.ex
+++ lib/elixir_security_advisories/dump.ex
@@ -1,41 +1,44 @@
 defmodule ElixirSecurityAdvisories.Dump do
+  @directory "packages"
+
   def dump(nodes) do
     nodes
     |> Enum.group_by(& &1["advisory"]["ghsaId"])
-    |> Enum.map(fn {_, vulnerabilities} ->
-      [vulnerability | _] = vulnerabilities
-      advisory = vulnerability["advisory"]
+    |> Enum.map(&map_vulnerabilities_to_advisories/1)
+    |> tap(fn _ -> File.rm_rf!(@directory) end)
+    |> Enum.map(&map_advisory_to_file/1)
+  end
 
-      disclosure_date =
-        advisory["publishedAt"]
-        |> case do
-          nil ->
-            ""
+  defp map_vulnerabilities_to_advisories({_, vulnerabilities = [%{"advisory" => advisory, "package" => package} | _]}) do
+    %{
+      id: advisory["ghsaId"],
+      package: package["name"],
+      disclosure_date: advisory_disclosure_date(advisory["publishedAt"]),
+      cve: advisory["ghsaId"],
+      link: advisory["permalink"],
+      title: advisory["summary"],
+      description: advisory["description"],
+      first_patched_versions: Enum.map(vulnerabilities, & &1["firstPatchedVersion"]["identifier"]),
+      vulnerable_version_ranges: Enum.map(vulnerabilities, & &1["vulnerableVersionRange"])
+    }
+  end
 
-          date ->
-            {:ok, disclosure_date, _} = DateTime.from_iso8601(date)
-            to_string(DateTime.to_date(disclosure_date))
-        end
+  defp advisory_disclosure_date(nil), do: ""
 
-      %{
-        id: advisory["ghsaId"],
-        package: vulnerability["package"]["name"],
-        disclosure_date: disclosure_date,
-        cve: advisory["ghsaId"],
-        link: advisory["permalink"],
-        title: advisory["summary"],
-        description: advisory["description"],
-        first_patched_versions: Enum.map(vulnerabilities, & &1["firstPatchedVersion"]["identifier"]),
-        vulnerable_version_ranges: Enum.map(vulnerabilities, & &1["vulnerableVersionRange"])
-      }
-    end)
-    |> tap(fn _ -> File.rm_rf!("packages") end)
-    |> Enum.map(fn advisory ->
-      path = Path.join(["packages", advisory.package, "#{advisory.id}.yml"])
-      content = Ymlr.document!(advisory)
+  defp advisory_disclosure_date(published_at) do
+    published_at
+    |> DateTime.from_iso8601()
+    |> case do
+      {:ok, date, _} -> to_string(DateTime.to_date(date))
+      _ -> ""
+    end
+  end
 
-      File.mkdir_p!(Path.dirname(path))
-      File.write!(path, content)
-    end)
+  defp map_advisory_to_file(advisory) do
+    path = Path.join([@directory, advisory.package, "#{advisory.id}.yml"])
+    content = Ymlr.document!(advisory)
+
+    File.mkdir_p!(Path.dirname(path))
+    File.write!(path, content)
   end
 end
